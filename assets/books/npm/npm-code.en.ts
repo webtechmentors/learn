7df4a03a5d14d2c12b6f164ad17c5a7809877685
Harry Cui <harrycui@Harrys-MacBook-Pro.local>
2023-09-28 16:30:41 -0400

    Add querystring.

---
+++ assets/books/npm/npm-code.en.ts
@@ -0,0 +1,142 @@
+export const npm_1_1_1 =
+`choco install nodejs`;
+
+export const npm_1_1_2 =
+`brew install node`;
+
+export const npm_1_1_3 =
+`sudo apt update`;
+
+export const npm_1_1_4 =
+`sudo apt install nodejs`;
+
+export const npm_1_1_5 =
+`sudo apt install npm`;
+
+export const npm_1_1_6 =
+`curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash`;
+
+export const npm_1_1_7 =
+`source ~/.bashrc`;
+
+export const npm_1_1_8 =
+`nvm install node`;
+
+export const npm_1_1_9 =
+`node -v`;
+
+export const npm_1_1_10 =
+`npm -v`;
+
+export const npm_2_1_1 =
+`mkdir my-new-project`;
+
+export const npm_2_1_2 =
+`cd my-new-project`;
+
+export const npm_2_1_3 =
+`npm init`;
+
+export const npm_2_1_4 =
+`npm init -y`;
+
+export const npm_2_1_5 =
+`{
+  "name": "my-new-project",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \\"Error: no test specified\\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}`;
+
+export const npm_2_1_6 =
+`touch index.js`;
+
+export const npm_2_2_1 =
+`"name": "my-project"`;
+
+export const npm_2_2_2 =
+`"version": "1.0.0"`;
+
+export const npm_2_2_3 =
+`"description": "A simple Node.js project"`;
+
+export const npm_2_2_4 =
+`"main": "index.js"`;
+
+export const npm_2_2_5 =
+`"author": "John Doe"`;
+
+export const npm_2_2_6 =
+`"author": {
+  "name": "John Doe",
+  "email": "john@example.com",
+  "url": "http://example.com"
+}`;
+
+export const npm_2_2_7 =
+`"license": "MIT"`;
+
+export const npm_2_2_8 =
+`"repository": {
+  "type": "git",
+  "url": "https://github.com/user/my-project.git"
+}`;
+
+export const npm_2_2_9 =
+`"dependencies": {
+  "express": "^4.17.1"
+}`;
+
+export const npm_2_2_10 =
+`"devDependencies": {
+  "mocha": "^9.1.0"
+}`;
+
+export const npm_2_2_11 =
+`"peerDependencies": {
+  "react": "^17.0.0"
+}`;
+
+export const npm_2_2_12 =
+`"optionalDependencies": {
+  "some-optional-package": "^1.0.0"
+}`;
+
+export const npm_2_2_13 =
+`"scripts": {
+  "start": "node index.js",
+  "test": "mocha"
+}`;
+
+export const npm_2_2_14 =
+`"engines": {
+  "node": ">=14.0.0"
+}`;
+
+export const npm_2_2_15 =
+`"private": true`;
+
+export const npm_2_2_16 =
+`"keywords": ["web", "express", "node"]`;
+
+export const npm_2_2_17 =
+`"homepage": "https://github.com/user/my-project#readme"`;
+
+export const npm_2_2_18 =
+`"bugs": {
+  "url": "https://github.com/user/my-project/issues"
+}`;
+
+export const npm_2_2_19 =
+`"bundledDependencies": ["some-package"]`;
+
+// export const npm_1_1_7 =
+// ``;
+
+// export const npm_1_1_1 =
+// ``;
